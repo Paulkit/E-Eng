06fe48bf94b43c15d2b54f3ac085c4e2d9ee1cf8
Anthony-PC\Anthony <anthony00600@yahoo.com.hk>
2016-11-06 20:27:03 +0800

    Menu designed

--- Assignment/app/src/main/java/com/example/anthony/assignment/MainActivity.java
+++ Assignment/app/src/main/java/com/example/anthony/assignment/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.anthony.assignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //https://ssl.gstatic.com/dictionary/static/sounds/de/0/[word].mp3
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showSyllabary(View view){
+
+    }
+
+    public void showQuiz(View view){
+
+    }
+
+    public void showHighScore(View view){
+
+    }
+
+    public void quit(View view) {
+        System.exit(0);
     }
 }
