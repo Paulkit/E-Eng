f6976fa8df8dbc25c889c7fb0b460e20634dbdb1
Michael Lam <lch101025michael@gmail.com>
2016-11-18 02:24:32 +0800

    Tutorial done

--- Assignment/app/src/main/java/com/example/anthony/assignment/TutorialsWordSelections.java
+++ Assignment/app/src/main/java/com/example/anthony/assignment/TutorialsWordSelections.java
@@ -1,10 +1,13 @@
 package com.example.anthony.assignment;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Xml;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,11 +16,16 @@ import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.xmlpull.v1.XmlSerializer;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -25,8 +33,9 @@ public class TutorialsWordSelections extends AppCompatActivity {
     String domain;
     ListView lv_TutorialsWords;
     ArrayList<WordData> words = new ArrayList<WordData>();
-    String[] name;
-    String[] wordID;
+    String[] name = {""};
+    String[] wordID ;
+    ArrayList<String> lst = new ArrayList<String>(Arrays.asList(name));
     String url ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +44,92 @@ public class TutorialsWordSelections extends AppCompatActivity {
         domain = getIntent().getStringExtra("domain");
         setTitle(domain);
         lv_TutorialsWords = (ListView) findViewById(R.id.lv_TutorialsWords);
-         url = "https://od-api.oxforddictionaries.com:443/api/v1/wordlist/en/lexicalCategory=noun,adjective;domains=" + domain;
+        url = "https://od-api.oxforddictionaries.com:443/api/v1/wordlist/en/lexicalCategory=noun,adjective;domains=" + domain;
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this , R.layout.list_item,lst);
+        lv_TutorialsWords.setAdapter(adapter);
+        new CallbackTask().execute(url);
 
     }
+
+    private class CallbackTask extends AsyncTask<String, String, String> {
+
+        ArrayAdapter<String> adapter;
+        @Override
+        protected String doInBackground(String... params) {
+            //TODO: replace with your own app id and app key
+            final String app_id = "6d1da903";
+            final String app_key = "610188df55f40e7dc8135da75a622b58";
+            try {
+                URL url = new URL(params[0]);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("Accept", "application/json");
+                urlConnection.setRequestProperty("app_id", app_id);
+                urlConnection.setRequestProperty("app_key", app_key);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                JSONObject json = new JSONObject(stringBuilder.toString());
+                JSONObject metadata = json.getJSONObject("metadata");
+                String total = metadata.getString("total");
+                Log.i("Total of words", total);
+
+                JSONArray results = json.getJSONArray("results");
+                wordID = new String[results.length()];
+                for (int i = 0; i < results.length(); i++) {
+                    JSONObject jsonobject = results.getJSONObject(i);
+                    words.add(new WordData(jsonobject.getString("id"), jsonobject.getString("word")));
+                    publishProgress(jsonobject.getString("word"));
+                    wordID[i]=jsonobject.getString("id");
+                }
+
+
+            } catch (Exception e) {
+
+            }
+                 return "Finish Async";
+        }
+        @Override
+        protected void onPreExecute() {
+           super.onPreExecute();
+            adapter = (ArrayAdapter<String>) lv_TutorialsWords.getAdapter();
+            adapter.clear();
+
+        }
+
+        protected void onProgressUpdate(String... progress) {
+          super.onProgressUpdate();
+            adapter.add(progress[0]);
+            adapter.notifyDataSetChanged ();
+            lv_TutorialsWords.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Log.i("Selected ID",wordID[position]);
+                    Log.i("Selected Word",lst.get(position));
+                     Intent i = new Intent(TutorialsWordSelections.this, TutorialsWord.class);
+                      i.putExtra("name",lst.get(position));
+                    i.putExtra("wordID",wordID[position]);
+                    startActivity(i);
+
+                }
+            });
+        }
+
+
+
+        @Override
+        protected void onPostExecute(String result) {
+          super.onPostExecute(result);
+
+            Log.i("post",result);
+        }
+    }
+
 
 }
 
